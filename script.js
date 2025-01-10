8b40917e64cee8fe4192a67fcfe50a8d8469614f
laryTgs <laryssatortato99@gmail.com>
2025-01-10 18:07:04 -0300

    att

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const img = document.querySelector('#logo');
 function diminuirMenu() {
 if (window.scrollY > 60) {
     menu.style.height = '110px';
-    img.style.height = '90px'; img.style.width = '90px'; img.style.margin = '10px 100px'
+    img.style.height = '90px'; img.style.width = '90px'; img.style.margin = '10px 100px'; 
 } else {
     menu.style.height = '500px'; 
     img.style.height = '150px'; img.style.width = '150px'; img.style.margin = '20px'
