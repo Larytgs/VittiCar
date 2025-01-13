ea36421099836d5e255e4094e7460c38555d4c68
laryTgs <laryssatortato99@gmail.com>
2025-01-13 19:04:24 -0300

    att

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function diminuirMenu() {
 if (window.scrollY > 60) {
     menu.style.height = '110px'; menu.style.position = 'fixed'; menu.style.backgroundColor = 'white'; 
     img.style.height = '90px'; img.style.width = '90px'; img.style.margin = '10px 100px';
-    main.style.margin = '10% 0%'
+    main.style.margin = '5% 0%'
 } else {
     menu.style.height = '700px'; menu.style.position = 'fixed'; //nao consigo deixar fixo o tamanho grande, logo quando entra no site
     img.style.height = '150px'; img.style.width = '150px'; img.style.margin = '20px';
