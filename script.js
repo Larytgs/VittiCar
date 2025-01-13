5939656cdb451080a1c4ed1a813555b8c3885cc8
laryTgs <laryssatortato99@gmail.com>
2025-01-13 15:06:07 -0300

    tentando arrumar o header

--- script.js
+++ script.js
@@ -8,9 +8,9 @@ const main = document.querySelector('#corpo');
 
 function diminuirMenu() {
 if (window.scrollY > 60) {
-    menu.style.height = '110px'; menu.style.position = 'fixed'; 
-    menu.style.backgroundColor = 'red'; main.style.margin = '50% 0%'
+    menu.style.height = '110px'; menu.style.position = 'fixed'; menu.style.backgroundColor = 'white'; 
     img.style.height = '90px'; img.style.width = '90px'; img.style.margin = '10px 100px';
+    main.style.margin = '10% 0%'
 } else {
     menu.style.height = '700px'; menu.style.position = 'fixed'; //nao consigo deixar fixo o tamanho grande, logo quando entra no site
     img.style.height = '150px'; img.style.width = '150px'; img.style.margin = '20px';
