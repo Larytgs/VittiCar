e512d2cb9991156309e40ef9a41b3016cd54dcf1
laryTgs <laryssatortato99@gmail.com>
2025-01-13 14:30:44 -0300

    att

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 /*Parte do header, diminui o tamanho do menu no momento em que houver scroll*/
+
 const menu = document.querySelector('#menu');
 const img = document.querySelector('#logo');
+const boton = document.querySelectorAll('.botoes');
+
+const main = document.querySelector('#corpo');
 
 function diminuirMenu() {
 if (window.scrollY > 60) {
-    menu.style.height = '110px';
-    img.style.height = '90px'; img.style.width = '90px'; img.style.margin = '10px 100px'; 
+    menu.style.height = '110px'; menu.style.position = 'fixed'; 
+    menu.style.backgroundColor = 'red'; main.style.margin = '50% 0%'
+    img.style.height = '90px'; img.style.width = '90px'; img.style.margin = '10px 100px';
 } else {
-    menu.style.height = '500px'; 
-    img.style.height = '150px'; img.style.width = '150px'; img.style.margin = '20px'
+    menu.style.height = '700px'; menu.style.position = 'fixed'; //nao consigo deixar fixo o tamanho grande, logo quando entra no site
+    img.style.height = '150px'; img.style.width = '150px'; img.style.margin = '20px';
 }
 }
 
